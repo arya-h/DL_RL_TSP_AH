2312a10f4d2dc30939311d5658d9739f3a2f71c5
Arya Houshmand <houshmarya@gmail.com>
2022-06-26 13:40:35 +0200

    added actor-critic whole directory

---
+++ actor_critic/actor_critic_training/data.py
@@ -0,0 +1,35 @@
+from inspect import CO_ASYNC_GENERATOR
+import torch
+from torch.utils.data.dataset import Dataset
+from torch.utils.data import DataLoader
+
+from env import Env_tsp
+from config import Config, load_pkl, pkl_parser
+
+class Generator(Dataset):
+	def __init__(self, cfg, env):
+		#will feed another set of nodes to test the speed of the training process
+
+		self.data = env.get_batch_nodes(cfg.n_samples)
+		
+	def __getitem__(self, idx):
+		return self.data[idx]
+
+	def __len__(self):
+		return self.data.size(0)
+
+if __name__ == '__main__':
+	# cfg = load_pkl(pkl_parser().path)
+	cfg = Config()
+	env = Env_tsp(cfg)
+	dataset = Generator(cfg, env)
+	data = next(iter(dataset))
+	print(data.size())
+	
+	device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+	dataloader = DataLoader(dataset, batch_size = cfg.batch, shuffle = True)
+	for i, data in enumerate(dataloader):
+		print(data.size())
+		print(data)
+		if i == 0:
+			break
